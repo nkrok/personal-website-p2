2647eac890bdb72a75d5033ba126b44301ac9aeb
cm8check <nkrok7@gmail.com>
2021-12-09 18:40:39 -0600

    Lint javascript and fix minor syntax issues

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
+"use strict";
+
 var featuredProjects = document.getElementById("featured-projects");
 var projectArticles = featuredProjects.getElementsByTagName("article");
 
 for (let i = 0; i < projectArticles.length; i++) {
   let proj = projectArticles[i];
-  proj.addEventListener("click", function() {onClickProject(proj)}, false);
+
+  proj.addEventListener("click", function() {
+    onClickProject(proj);
+  }, false);
 }
 
 function onClickProject(articleObj) {
